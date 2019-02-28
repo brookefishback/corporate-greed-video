b4d771a46879b73db6fd4ce20f4326334f808d35
Brooke Fishback <36209055+brookefishback@users.noreply.github.com>
2019-02-27 22:21:31 -0600

    Delete CorporateGreedVideo.py

--- CorporateGreedVideo.py
+++
@@ -1,7 +0,0 @@
-#I want a point of sale solution for Family Video’s rivals
-#At very minimum it needs to have a stock of 5 rentable titles
-#Employee should be able to register new users, rent titles to users, and check titles back in
-#Any other functionality is secondary but welcome.
-#I don’t care how you implement checking in/out, but no shortcuts/fakery
-
-
